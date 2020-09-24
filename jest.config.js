68b3d98c8c3b93c9ee1bb7532de796ebc4626952
Villem Alango <villem.alango@gmail.com>
2020-09-24 17:46:28 +0300

    edit: jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   bail: 1,
   collectCoverage: true,
-  collectCoverageFrom: ['index.js', 'src/*.js', 'benchmarks/simple*.js'],
+  collectCoverageFrom: ['src/*.js', '!src/index.js'],
   coverageDirectory: 'reports',
   coveragePathIgnorePatterns: ['reports', 'test'],
   // coverageProvider: 'babel',
