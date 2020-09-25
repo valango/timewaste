b64301d1b4271c3dc8ddd2b21f8a4fb658867b64
Villem Alango <villem.alango@gmail.com>
2020-09-25 13:35:53 +0300

    small fix in: benchmarks/run.js

--- benchmarks/run.js
+++ benchmarks/run.js
@@ -1,28 +1,18 @@
 //  For debugging IDE.
 'use strict'
 
-const Q = require('../src/Qektors')
-const S = require('../src/Qektors-strict')
-const profiler = require('../src')
+const profiler = require('..')
 const run = require('../test/run')
-
+let res
 
 const N_THREADS = 2
 // const tags = 'abcdefghij'.split(''), nTags = tags.length
 const tags = 'ab'.split(''), nTags = tags.length
 
 let options = { profiler, tags, threads: N_THREADS - 1 }
-let res = new S({ width: 3 })
-res.push(11,12)
-let at = res.at(1)
-let v = at.length
-v = at.indexOf(12)
-v = at[2]
-at[2] = 25
-v = at[2]
 
 res = run({
-  fuck: (cycle, it) => {
+  failAt: (cycle, it) => {
     if (cycle === (nTags + 2) && it === 1) {
       return true
     }
